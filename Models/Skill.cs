55b188c2becc89dde5874bd6eeb3b510a1c11f98
Andy Cox <apcox89@gmail.com>
2022-05-01 10:57:30 -0500

    Add project files.

---
+++ Models/Skill.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portals_Technoprolis_RPG.Models
+{
+    [Serializable]
+    //class to act as a menu for player skill upgrades
+    //the Player can dynamically assign upon quest completion
+    //using the PlayerSkill "strategy" design method
+    public class Skill : IEquatable<Skill>
+    {
+        //a simple class to instantiate a list of skills assigned to a Player
+        public int SkillID { get; set; }
+        public string SkillName { get; set; }
+
+        public Skill(int skillID, string skillName)
+        {
+            SkillID = skillID;
+            SkillName = skillName;
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + SkillID + "   Name: " + SkillName;
+        }
+
+        public override int GetHashCode()
+        {
+            return SkillID;
+        }
+        public bool Equals(Skill other)
+        {
+            if (other == null) return false;
+            return (this.SkillID.Equals(other.SkillID));
+        }
+    }
+}
