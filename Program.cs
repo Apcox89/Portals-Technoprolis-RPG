f2ae8e417e954a81b4b7d84b310ca6c05c9d5e6a
Apcox89 <apcox89@gmail.com>
2024-08-31 13:09:53 -0500

    Db-Context-Init

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using Portals_Technoprolis_RPG.Models;
-using Portals_Technoprolis_RPG.Activities;
 using Portals_Technoprolis_RPG.GameEngine;
+using Microsoft.Extensions.DependencyInjection;
+using Portals_Technoprolis_RPG.Database;
+using Microsoft.EntityFrameworkCore;
 
 /*
  * Andy Cox
@@ -27,7 +28,7 @@ using Portals_Technoprolis_RPG.GameEngine;
  * at ln150 where the Skill chosen by the Player dynamically is assigned to their Skill-class.
  */
 
-//May-22-Update:
+//Sept-24-Update:
 /*
  ==> Need to abstract logic and add real data
     + working off branching struct for upcoming features
@@ -42,6 +43,42 @@ namespace Portals_Technoprolis_RPG
         {
             Game game = new Game();
             game.Start();
+            //Cox note: need to abstract the server-string away into a secrets file
+            var serviceProvider = ServiceHelper.ConfigureServices();
+
+            // Now you can resolve your DbContext and use it
+            using (var context = serviceProvider.GetRequiredService<PortalsDbContext>())
+            {
+                // Your code to use the context --ex:
+                //var data = context.YourEntity.ToList();
+                //Console.WriteLine(data);
+            }
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Load configuration from user secrets if in development mode
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Configuration.AddUserSecrets<Program>();
+            }
+
+            // Add services to the container.
+            builder.Services.AddDbContext<PortalsDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("PortalsDb")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.Run();
+
         }
 
     }
---
+++ Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Portals_Technoprolis_RPG.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portals_Technoprolis_RPG
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<PortalsDbContext>(options =>
+                options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Portals;Trusted_Connection=True;"));
+
+            // Other service registrations
+        }
+
+        // Other methods...
+    }
+}
